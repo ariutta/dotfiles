52ecb15b9575446bbb75dcae7608d8efb201a649
Anders Riutta <git@andersriutta.com>
2018-06-21 14:27:19 -0700

    Should now install desktop and cli

--- custom/pathvisio/default.nix
+++ custom/pathvisio/default.nix
@@ -3,7 +3,12 @@
 
 # some of the CLI args allowed
 # https://github.com/PathVisio/pathvisio/blob/master/modules/org.pathvisio.launcher/src/org/pathvisio/launcher/PathVisioMain.java
-{ stdenv, fetchurl, fetchFromGitHub, makeDesktopItem, unzip, ant, jdk }:
+{ stdenv, fetchurl, fetchFromGitHub, makeDesktopItem, unzip, ant, jdk, options ? {
+  desktop = true;
+  interactions = false;
+  metabolites = false;
+  # TODO look into downloading the dbs, if requested
+} }:
 
 let
   baseName = "PathVisio";
@@ -17,12 +22,6 @@ stdenv.mkDerivation rec {
 
   javaPath = "${jdk.jre}/bin/java";
 
-#  src = fetchurl {
-#    url = "https://www.pathvisio.org/data/releases/current/pathvisio_bin-${version}.zip";
-#    sha256 = "0wpnxkc9fs9h3n0p5y7j8b28gpvyhac4kw0vbqi9j5q6hphi0fs0";
-#  };
-
-  # TODO build from source instead of downloading binary
   src = fetchFromGitHub {
     owner = "pathvisio";
     repo = "PathVisio";
@@ -30,60 +29,73 @@ stdenv.mkDerivation rec {
     sha256 = "1n2897290g6kph1l04d2lj6n7137w0gnavzp9rjz43hi1ggyw6f9";
   };
 
-  srcPngIcon = fetchurl {
-    url = "https://www.pathvisio.org/wcms/wp-content/uploads/2013/03/pathvisio-eye-switch-e1363105300175.png";
-    sha256 = "1ywi2d4iyg1ppgmmclzamanqlhihk8dad54qfx7cl3405y80bqir";
-  };
-
   srcBiopax3GPML = fetchurl {
     url = "https://cdn.rawgit.com/wikipathways/wikipathways.org/e8fae01e/wpi/bin/Biopax3GPML.jar";
     sha256 = "1jm5khh6n78fghd7wp0m5dcb6s2zp23pgsbw56rpajfxgx1sz7lg";
   };
 
-#  # TODO use this https://github.com/PathVisio/pathvisio/blob/master/Info.plist
-#  plistSrc = ./Info.plist;
-#
-#  iconSrc = fetchurl {
-#    url = "https://cdn.rawgit.com/PathVisio/pathvisio/61f15de9/lib-build/bigcateye.icns";
-#    sha256 = "1ygjcjbnabilfxvzbaamzr739f2b8l1gy56jnq2lxk9i2nxkqgxq";
-#  };
-#
-#  bridgerestSrc = fetchurl {
-#    url = "https://cdn.rawgit.com/PathVisio/pathvisio/61f15de9/lib/org.bridgedb.webservice.bridgerest.jar";
-#    sha256 = "1s3xmvqpm6m6x70hn4mlfvspkrsmlj2ixba9hfgpsr9q5v144d8z";
-#  };
+  srcPngIcon = "${src}/www/bigcateye_135x135.png";
 
-  # sh ./result/bin/pathvisio -p ./org.bridgedb.webservice.bridgerest.jar
+  iconSrc = "${src}/lib-build/bigcateye.icns";
 
 #  # WARNING: this is very large
-#  metaboliteSrc = fetchurl {
+#  srcMetabolites = fetchurl {
 #    url = "http://bridgedb.org/data/gene_database/metabolites_20180508.bridge.zip";
 #    sha256 = "13l3p755k3k4vp2zg24fbsmprbgml75cg105qmzs85xipz92cqv4";
 #  };
 
+#  srcInteractions = fetchurl {
+#    url = "http://developers.pathvisio.org/data/gene_database/interactions_20180425.bridge";
+#    sha256 = "0bijs9jvl0362l96ar11zndziwlccyxysdqlbglwl34p733x1p64";
+#  };
+
   postPatch = ''
-    substituteInPlace ./pathvisio.sh \
-          --replace "java" "${javaPath}" \
-          --replace "cd" "#cd" \
-          --replace "pathvisio.jar" "$out/lib/pathvisio/pathvisio.jar"
+    for f in $out/bin/*; do
+      substituteInPlace $out/scripts/* \
+            --replace "java -ea" "${javaPath} -ea"
+    done
   '';
 
-  buildPhase = "ant";
-  #buildPhase = "ant zip";
-  #buildPhase = "ant core.jar";
-  #buildPhase = "ant launcher.jar";
+  buildPhase = (if ! options.desktop then "ant"
+  else if stdenv.system == "x86_64-darwin" then "ant appbundler"
+  else "ant exe") + ''
 
-#  postBuild = ''
-#    echo "****************** postBuild "******************"
-#    echo "ls -la ./"
-#    ls -la ./
-#  '';
+    mkdir -p ./bin
+    cp ./scripts/gpmldiff.sh ./bin/gpmldiff
+    cp ./scripts/gpmlpatch.sh ./bin/gpmlpatch
+
+    cat > ./bin/gpmlconvert <<EOF
+    #! $shell
+    CLASSPATH=../modules/org.pathvisio.core.jar:../lib/*:${srcBiopax3GPML}
+    exec ${javaPath} -ea -classpath \$CLASSPATH org.pathvisio.core.util.Converter "\$@"
+    EOF
+
+    chmod a+x ./bin/gpmlconvert
+    chmod a+x ./bin/gpmldiff
+    chmod a+x ./bin/gpmlpatch
+  '';
+
+  doCheck = true;
+
+  checkPhase = ''
+    # TODO are we running the existing tests?
+    cd ./bin
+    ./gpmlconvert ../example-data/Hs_Apoptosis.gpml Hs_Apoptosis.pdf
+    rm Hs_Apoptosis.pdf
+    ./gpmlconvert ../example-data/Hs_Apoptosis.gpml Hs_Apoptosis.png
+    rm Hs_Apoptosis.png
+    ./gpmlconvert ../example-data/Hs_Apoptosis.gpml Hs_Apoptosis.owl
+    rm Hs_Apoptosis.owl
+    ./gpmldiff ../example-data/Hs_Apoptosis.gpml ../example-data/Hs_Cell_cycle_KEGG.gpml
+    rm Hs_Apoptosis.bpss
+    cd ../
+  '';
 
   # TODO is this just for Linux?
   desktopItem = makeDesktopItem {
     name = name;
     exec = ''
-      ${javaPath} -jar -Dfile.encoding=UTF-8 $out/lib/pathvisio/pathvisio.jar "\$@"
+      ${javaPath} -jar -Dfile.encoding=UTF-8 $out/lib/pathvisio.jar "\$@"
     '';
     icon = "${srcPngIcon}";
     desktopName = baseName;
@@ -96,64 +108,42 @@ stdenv.mkDerivation rec {
   };
 
   installPhase = ''
-    echo '****************** installPhase "******************'
-    pwd
-    echo 'ls -la ./'
-    ls -la ./
-    mkdir -p $out/lib/pathvisio $out/bin
-    cp -r ./ $out/
+    mkdir -p $out/bin $out/lib $out/modules $out/share
 
-    cat > $out/bin/gpmlconverter <<EOF
-    #! $shell
-    CLASSPATH=$out/modules/org.pathvisio.core.jar:$out/lib/*:${srcBiopax3GPML}
-    exec ${jdk.jre}/bin/java -ea -classpath \$CLASSPATH org.pathvisio.core.util.Converter "\$@"
-    EOF
+    cp -r ./bin/* $out/bin/
+    for f in $out/bin/*; do
+      substituteInPlace $f \
+            --replace "../lib" "$out/lib" \
+            --replace "../modules" "$out/modules"
+    done
 
-    chmod a+x $out/bin/gpmlconverter
+    cp -r ./lib/* $out/lib/
+    cp -r ./modules/* $out/modules/
 
-    echo 'try'
-    echo './result/bin/gpmlconverter WP1229_71445.gpml png.png'
+#    echo 'Sample commands'
+#    echo 'wget https://cdn.rawgit.com/PathVisio/GPML/fa76a73d/test/2013a/WP1243_69897.gpml'
+#    echo 'GPML -> BioPAX/OWL'
+#    echo 'gpmlconvert WP1243_69897.gpml WP1243_69897.owl'
+#    echo 'GPML -> PDF'
+#    echo '/bin/gpmlconvert WP1243_69897.gpml WP1243_69897.pdf'
+#    echo 'GPML -> PNG'
+#    echo 'gpmlconvert WP1243_69897.gpml WP1243_69897.png'
   '' + (
-  if stdenv.system == "x86_64-darwin" then ''
-    #mkdir -p "$out/Applications/${baseName}.app/Contents/MacOS" "$out/Applications/${baseName}.app/Contents/Resources"
-
-    echo '****************** </installPhase> "******************'
-    pwd
-
-    echo 'ls -la ./'
-    ls -la ./
-
-    echo 'ls -la $out'
-    ls -la $out
-
-    echo 'ls -la ./release'
-    ls -la ./release
+  if ! options.desktop then ''
+    echo 'desktop functionality not enabled'
+  '' else if stdenv.system == "x86_64-darwin" then ''
+    mkdir -p $out/share/Applications
+    unzip -o release/${baseName}.app.zip -d $out/share/Applications/
   '' else ''
+    mkdir -p $out/share/applications
     ln -s ${desktopItem}/share/applications/* $out/share/applications/
   '');
-
-#  installPhase = ''
-#    mkdir -p $out/lib/pathvisio $out/bin
-#    cp -r ./pathvisio-${version}/* $out/lib/pathvisio
-#    ln -s $out/lib/pathvisio/pathvisio.sh $out/bin/pathvisio
-#    chmod a+x "$out/bin/pathvisio"
-#  '' + (
-#  if stdenv.system == "x86_64-darwin" then ''
-#    mkdir -p "$out/Applications/${baseName}.app/Contents/MacOS" "$out/Applications/${baseName}.app/Contents/Resources"
-#    cp $out/lib/pathvisio/pathvisio.sh "$out/Applications/${baseName}.app/Contents/MacOS/${baseName}"
-#    cp ${iconSrc} "$out/Applications/${baseName}.app/Contents/Resources/${baseName}.icns"
-#    cp ${plistSrc} "$out/Applications/${baseName}.app/Contents/Info.plist"
-#    chmod a+x "$out/Applications/${baseName}.app/Contents/MacOS/${baseName}"
-#  '' else ''
-#    ln -s ${desktopItem}/share/applications/* $out/share/applications/
-#  '');
 
   meta = with stdenv.lib;
     { description = "A tool to edit and analyze biological pathways";
       homepage = https://www.pathvisio.org/;
       license = licenses.asl20;
       maintainers = with maintainers; [ ];
-      #platforms = with platforms; [ linux darwin ];
       platforms = platforms.all;
     };
 }
